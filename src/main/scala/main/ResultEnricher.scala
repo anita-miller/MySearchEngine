8471ec1a98c84c7fa489b645b248cb75a4c13353
Anita Miller <anaseri@zendesk.com>
2021-09-02 13:00:29 +1000

    resultEnricher

--- src/main/scala/main/ResultEnricher.scala
+++ src/main/scala/main/ResultEnricher.scala
@@ -1,8 +1,51 @@
 package main
 
-import io.circe.Json
+import io.circe.{Json, JsonObject}
 
 object ResultEnricher {
   val fieldToEnrich = List("organisation_id", "submitter_id", "assignee_id")
-  def apply(result: List[Json], orgIndex: DocumentIndex, userIndex: DocumentIndex, ticketIndex: DocumentIndex) : List[Json] = ???
+  def apply(result: List[Json], orgIndex: DocumentIndex, userIndex: DocumentIndex, ticketIndex: DocumentIndex) : List[Json] = {
+    // go through result => base on which field => choose the index
+    result.map {jsonObject =>
+      val listOfValues: Seq[String] = indexDocument(jsonObject, "submitter_id") match {
+        case Some(jsonObj) => jsonToListofString(jsonObj)
+        case None => List("")
+      }
+      val correspondingUserJson = listOfValues.flatMap { id =>
+        userIndex.index.get("_id") match {
+          case Some(index) => index.search(id)
+          case None => List.empty
+        }
+      }
+      val maybeUserName: Option[String] = correspondingUserJson.headOption.flatMap { jsonObjectAgain =>
+        jsonObjectAgain.hcursor.get[String]("name").toOption
+      }
+      val maybejson = maybeUserName match {
+        case Some(userName) => jsonObject.asObject.map(json => json.add("submitter", Json.fromString(userName)))
+        case None =>jsonObject.asObject.map(json => json.add("submitter", Json.fromString("Could not find")))
+      }
+      maybejson match {
+        case Some(json) => Json.fromJsonObject(json)
+        case None =>Json.fromJsonObject(JsonObject.empty)
+      }
+    }
+  }
+
+  private def jsonToListofString(jsonval: Json): List[String] = {
+    jsonval.fold(
+      List.empty,
+      boolVal => List(boolVal.toString),
+      numval => List(numval.toString),
+      stringval => List(stringval),
+      arrayVal => arrayVal.toList.flatMap(jsondoc => jsonToListofString(jsondoc)),
+      objectVal => List(objectVal.toString)
+    )
+  }
+
+  private def indexDocument(document: Json, field: String): Option[Json] = {
+    document.asObject match {
+      case Some(jsonObject) => jsonObject.apply(field)
+      case None => None
+    }
+  }
 }
--- src/test/scala/main/ResultEncricherSpec.scala
+++ src/test/scala/main/ResultEncricherSpec.scala
@@ -7,14 +7,40 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class ResultEncricherSpec  extends AnyFunSpec{
 
-  val userIndex = DocumentIndex (Map("_id" -> 456, "name" -> "Anita"), List("_id", "name"))
+  val userIndex = IndexBuilder.createIndexes(List("_id", "name"), List(Json.obj("_id" := 456, "name":= "Anita")))
+
   describe("ResultEnricher") {
-    it("enriches ticket documents") {
-      val ticket = Json.obj("id" := "123", "submitter_id":= 456)
+    describe("enriches tickets") {
+      it("enriches ticket documents") {
+        val ticket = Json.obj("id" := "123", "submitter_id" := 456)
+        val enriched = ResultEnricher(List(ticket), userIndex, userIndex, userIndex)
 
-      val enriched = ResultEnricher(List(ticket))
+        enriched shouldBe List(Json.obj("id" := "123", "submitter_id" := 456, "submitter" := "Anita"))
+      }
 
-      enriched shouldBe List(Json.obj("id" := "123", "submitter_id":= 456,"submitter":= "Anita"))
+      it("handles missing joins") {
+        val ticket = Json.obj("id" := "123", "submitter_id" := 789)
+
+        val enriched = ResultEnricher(List(ticket), userIndex, userIndex, userIndex)
+
+        enriched shouldBe List(Json.obj("id" := "123", "submitter_id" := 789, "submitter" := "Could not find"))
+      }
+    }
+
+    describe("enriches users") {
+      it("enriches user documents") {
+        val user = Json.obj("id" := "123", "organization_id" := 1111)
+        val enriched = ResultEnricher(List(user), userIndex, userIndex, userIndex)
+
+        enriched shouldBe List(Json.obj("id" := "123", "organization_id" := 1111, "organization" := "Zendesk"))
+      }
+
+      it("handles missing joins") {
+        val user = Json.obj("id" := "123", "organization_id" := 2222)
+        val enriched = ResultEnricher(List(user), userIndex, userIndex, userIndex)
+
+        enriched shouldBe List(Json.obj("id" := "123", "organization_id" := 2222, "organization" := "Could not find"))
+      }
     }
   }
 }
